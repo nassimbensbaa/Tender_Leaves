2151d1754391fe3c809036fe7f2de9cbf8d19c87
nassimbensbaa <150660547+nassimbensbaa@users.noreply.github.com>
2026-07-13 15:47:07 +0100

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
 
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
 
     res.status(200).json({
 
