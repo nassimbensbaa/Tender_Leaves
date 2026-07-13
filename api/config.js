3e3d7581835c12b5c96ba2493739b1e47aa2a30d
nassimbensbaa <150660547+nassimbensbaa@users.noreply.github.com>
2026-07-13 15:31:15 +0100

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,1 +1,11 @@
+export default function handler(req, res) {
 
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    res.status(200).json({
+
+        META_PIXEL_ID: process.env.META_PIXEL_ID || ""
+
+    });
+
+}
